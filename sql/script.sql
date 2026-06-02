a7593b7b81450c3fe3daec744a3b7775941deefd
viniciusdolzan-unidavi <vinicius.dolzan@unidavi.edu.br>
2026-06-02 20:52:31 -0300

    Update script.sql

--- sql/script.sql
+++ sql/script.sql
@@ -39,14 +39,18 @@ data_verificacao DATE
 INSERT INTO pontos_coleta (nome_local, endereco, bairro, cidade, contato)
 VALUES
 ('Secretaria de Agricultura e Meio Ambiente', 'Rua Expedicionário Rafael Busarello, 530', 'Padre Eduardo', 'Taió', '(47) 3562-8314'),
-('Hztech Informática', 'Avenida Franz Xavier Mainhardt, 450', 'Padre Eduardo', 'Taió', '(47) 3562-2008');
-('Secretaria Municipal de Meio Ambiente', 'Rua 11 de Março, 11', 'Centro', 'Ibirama', '(47) 3357-8500');
-('Vivo', 'Rua Tiradentes, 324', 'Centro', 'Ibírama', '(47) 3300-2419');
-('Ecoponto', 'Rua Dante Zonta, s/n', 'Estação', 'Ascurra', '(47) 3380-1345');
-('Decosu Tecnológica', 'Rua Lúcio Marchi, 140', 'Saltinho', 'Ascurra', '(47) 9617-3036');
+('Hztech Informática', 'Avenida Franz Xavier Mainhardt, 450', 'Padre Eduardo', 'Taió', '(47) 3562-2008'),
+('Secretaria Municipal de Meio Ambiente', 'Rua 11 de Março, 11', 'Centro', 'Ibirama', '(47) 3357-8500'),
+('Vivo', 'Rua Tiradentes, 324', 'Centro', 'Ibírama', '(47) 3300-2419'),
+('Ecoponto', 'Rua Dante Zonta, s/n', 'Estação', 'Ascurra', '(47) 3380-1345'),
+('Decosu Tecnológica', 'Rua Lúcio Marchi, 140', 'Saltinho', 'Ascurra', '(47) 9617-3036'),
 ('Ecoponto', 'Rua 14 de Março, s/n', 'Centro', 'Rodeio', '(47) 3384-0161');
 
-
+INSERT INTO materiais_aceitos (id_ponto, tipo_material, aceita)
+VALUES 
+(1, 'Computadores', TRUE), 
+(1, 'Baterias', FALSE), 
+(2, 'Celulares', TRUE);
 
 
 
